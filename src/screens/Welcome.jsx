f60252f0a924f0bfd8411adc97386d3c93e8aa7a
Ian Graham <i4ng1999@gmail.com>
2025-02-21 19:23:49 +0000

    Add Welcome screen background image

--- src/screens/Welcome.jsx
+++ src/screens/Welcome.jsx
@@ -1,9 +1,10 @@
-import { SafeAreaView, Text, View, Alert, StatusBar } from 'react-native'
+import { SafeAreaView, Text, View, Alert, StatusBar, ImageBackground, Platform } from 'react-native'
 import { useState, useContext } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { UserContext } from '../contexts/UserContext'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import FormButton from '../components/FormButton'
+import WelcomeImage from '../../assets/welcome.png'
 
 const Welcome = () => {
   const { loggedInUser } = useContext(UserContext)
@@ -33,17 +34,30 @@ const Welcome = () => {
   };
 
   return (
-    <SafeAreaView className="flex-1 items-center justify-around bg-slate-900">
-      <StatusBar barStyle="light-content" />
-      <Text className="text-3xl pt-10 text-white">Welcome {loggedInUser.username}</Text>
+    <SafeAreaView style={container} className="flex-1">
+      <StatusBar barStyle="dark-content" />
 
-      <View className="flex-1 justify-center w-full">
-        <FormButton buttonColour="bg-green-500" buttonText="View Meals" pressHandler={() => Alert.alert("My Meals")} />
-        <FormButton buttonColour="bg-blue-500" buttonText="Settings" pressHandler={() => Alert.alert("My Settings")} />
-      </View>
-      <FormButton buttonColour="bg-red-500" buttonText="Logout" pressHandler={confirmLogout} />
+      <ImageBackground
+        source={WelcomeImage}
+        resizeMode="cover"
+        className="flex-1 justify-evenly">
+
+        <View className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50" />
+
+        <Text className="text-3xl pt-10 text-center text-white">Welcome {loggedInUser.username}</Text>
+
+        <View className="flex-1 justify-center w-full">
+          <FormButton buttonColour="bg-green-500" buttonText="View Meals" pressHandler={() => Alert.alert("My Meals")} />
+          <FormButton buttonColour="bg-blue-500" buttonText="Settings" pressHandler={() => Alert.alert("My Settings")} />
+        </View>
+        <FormButton buttonColour="bg-red-500" buttonText="Logout" pressHandler={confirmLogout} />
+      </ImageBackground>
     </SafeAreaView>
   )
 }
+
+const container = {
+  paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+};
 
 export default Welcome
